0096953eb17b39a83f64332d9113490701a2ec4d
João Paulo <joaopaulofilho@gmail.com>
2016-12-16 16:01:55 -0200

    crud de usuários movido para controller usuarios

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -10,49 +10,6 @@ Class HomeController extends AppController {
     }
 
     public function index(){
-        // route
-        $this->action('lista_usuarios');
-    }
-
-    public function lista_usuarios(){
-
-        $this->vars(array(
-            'usuarios' => $this->UsuariosModel->all(),
-        ));
-    }
-
-    public function crud_usuario(){
-        $usuarios_id = $this->get['id'];
-
-        if(!empty($this->post)){
-            if(!empty($this->post['delete_usuarios_id'])){
-                $this->UsuariosModel->delete($this->post['delete_usuarios_id']);
-                echo json_decode(array('data'=>true));
-            } else
-
-            if(empty($usuarios_id)){
-                $this->UsuariosModel->insert($this->post);
-            } else 
-
-            if(!empty($usuarios_id)){
-                $this->UsuariosModel->update($usuarios_id, $this->post);
-            }
-
-
-            if($this->UsuariosModel->id() > 0){
-                // sucesso
-                $this->vars(array('message' => 'Usuário cadastrado com sucesso.'));
-            } else {
-                // falha
-                $this->vars(array('message' => 'Ocorreu erro ao cadastrar usuário.'));
-            }
-
-            $this->redirect('/');
-        }
-
-        $this->vars(array(
-            'usuario' => $this->UsuariosModel->first($usuarios_id),
-        ));
     }
 
     public function debug(){
