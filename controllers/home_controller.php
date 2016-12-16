0dc2c72fd46c7bd54ede88d72f6741ec877c43e0
João Paulo <joaopaulofilho@gmail.com>
2016-12-16 14:18:46 -0200

    principal (e único) controller para este projeto

---
+++ controllers/home_controller.php
@@ -0,0 +1,54 @@
+<?php
+
+Class HomeController extends AppController {
+
+    public function __construct(){
+        parent::__construct();
+        $this->loadModel(array(
+            'UsuariosModel' => 'usuarios_model',
+        ));
+    }
+
+    public function index(){
+        // route
+        $this->action('lista_usuarios');
+    }
+
+    public function lista_usuarios(){
+
+        $this->vars(array(
+            'usuarios' => $this->UsuariosModel->all(),
+        ));
+    }
+
+    public function crud_usuario(){
+        $usuarios_id = $this->get['usuarios_id'];
+
+        if(!empty($this->post)){
+            if(empty($usuarios_id)){
+                $this->UsuariosModel->insert($this->post);
+            } else 
+
+            if(!empty($this->post['delete_usuarios_id'])){
+                $this->UsuariosModel->delete($this->post['delete_usuarios_id']);
+            } else
+
+            if(!empty($usuarios_id)){
+                $this->UsuariosModel->update($usuarios_id, $this->post);
+            }
+
+
+            if($this->UsuariosModel->id() > 0){
+                // sucesso
+                $this->vars(array('message' => 'Usuário cadastrado com sucesso.'));
+            } else {
+                // falha
+                $this->vars(array('message' => 'Ocorreu erro ao cadastrar usuário.'));
+            }
+        }
+
+        $this->vars(array(
+            'usuario' => $this->UsuariosModel->first($usuarios_id),
+        ));
+    }
+}
