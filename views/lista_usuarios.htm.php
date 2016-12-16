3eb8bf9ab250f38bc5e7e45b5f13b1e5f1ccf044
João Paulo <joaopaulofilho@gmail.com>
2016-12-16 16:14:31 -0200

    bugfix corrigido endereços de crud usuários

--- views/lista_usuarios.htm.php
+++ views/lista_usuarios.htm.php
@@ -20,7 +20,7 @@ if(!empty($usuarios)){
             <td>'.$usuario['username'].'</td>
             <td>'.(!empty($usuario['passwd']) ? 'sim' : 'não').'</td>
             <td align="right">
-                <a href="'.Mapper::url('/home/crud-usuario/'.$usuario['id']).'" class="btn btn-xs btn-warning btn-edit"><i class="fa fa-pencil"></i> Editar</a>
+                <a href="'.Mapper::url('/usuarios/crud-usuario/'.$usuario['id']).'" class="btn btn-xs btn-warning btn-edit"><i class="fa fa-pencil"></i> Editar</a>
                 <a rel="'.$usuario['id'].'" class="btn btn-xs btn-danger btn-delete"><i class="fa fa-trash"></i> Excluir</a>
             </td>
         </tr>';
@@ -31,14 +31,14 @@ $tabela .= '</table>';
 <h3>Gestão de Usuários</h3>
 <div><strong>Lista de Usuários</strong></div>
 
-<div align="right"><a href="<?php echo Mapper::url('/home/crud-usuario'); ?>" class="btn btn-primary btn-sm">Inserir Usuário</a></div>
+<div align="right"><a href="<?php echo Mapper::url('/usuarios/crud-usuario'); ?>" class="btn btn-primary btn-sm">Inserir Usuário</a></div>
 <?php echo $tabela; ?>
 
 <script type='text/javascript'>
     $(function (){
         $('.btn-delete').click(function (){
             if(confirm('Tem certeza de que deseja excluir este usuário?')){
-                $.post("<?php echo Mapper::url('/home/crud-usuario'); ?>", {delete_usuarios_id: $(this).attr('rel')}, function (result){
+                $.post("<?php echo Mapper::url('/usuarios/crud-usuario'); ?>", {delete_usuarios_id: $(this).attr('rel')}, function (result){
                     window.location.href = window.location.href;
                 }, 'json');
             }
