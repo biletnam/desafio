4e9e10f68f4a7e1f2d202b3a1f555e0c0a8f7ac7
João Paulo <joaopaulofilho@gmail.com>
2016-12-16 23:22:07 -0200

    exibe mensagem de erro adequado

--- views/login.htm.php
+++ views/login.htm.php
@@ -10,7 +10,7 @@
 
             <div class="alert alert-dismissible alert-danger" style="display:none;">
                 <button type="button" class="close" data-dismiss="alert">&times;</button>
-                <strong>Oh snap!</strong> <a href="#" class="alert-link">Change a few things up</a> and try submitting again.
+                <strong>Erro:</strong> username ou senha incorretos.
             </div>
 
             <div class="form-group">
