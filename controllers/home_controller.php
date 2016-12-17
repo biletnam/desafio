77ba273b95b745dc3b33b6aafbb72aef9406fe99
João Paulo <joaopaulofilho@gmail.com>
2016-12-16 23:51:22 -0200

    validações de reservas

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -33,6 +33,10 @@ Class HomeController extends AppController {
 
     public function debug(){
         $this->loadModel('ReservasModel', 'reservas_model');
-        die(debug($this->ReservasModel->countBySalasId(2)));
+        die(debug(
+            $this->ReservasModel->insert(5, '16:00', 11), // validação 1
+            $this->ReservasModel->insert(2, '10:00', 09), // validação 2
+            $this->ReservasModel->insert(4, '16:00', 11)  // validação 3
+        ));
     }
 }
